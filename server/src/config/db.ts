c3e12193a085097fd45b9e4b6a5ecaef9703df49
alexia <alucor18@gmail.com>
2025-10-21 18:18:00 -0300

    railway

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -3,13 +3,4 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const pool = mysql.createPool({
-  host: process.env.DB_HOST,          
-  user: process.env.DB_USER,          
-  password: process.env.DB_PASSWORD,  
-  database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT),      
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-});
+export const pool = mysql.createPool(process.env.MYSQL_URL_PUBLIC as string);
